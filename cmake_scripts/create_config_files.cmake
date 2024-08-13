9ab2fdb0927fb09d55816cd6f3f70804bd95c754
lapochnihivan@gmail.com <lapochnihivan@gmail.com>
2024-08-13 13:51:46 +0300

    [f] cast_and_assignment: Fix paths for config file

--- cmake_scripts/create_config_files.cmake
+++ cmake_scripts/create_config_files.cmake
@@ -19,29 +19,32 @@ function(create_config_files)
     include(GNUInstallDirs)
     include(CMakePackageConfigHelpers)
 
-    set(CONFIG_DIR "${CMAKE_SOURCE_DIR}/lib/cmake/DynamicObject")
-    set(
-        ${ARG_DEST_CONFIG_INSTALL_DIR}
-        "${CMAKE_INSTALL_LIBDIR}/cmake/DynamicObject"
-    )
+    set(CONFIG_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/DynamicObject")
+    set(CONFIG_INSTALL_DIR ${CONFIG_DIR})
+    set(CONFIG_GENERATE_DIR ${CMAKE_SOURCES_DIR}/${CONFIG_DIR})
 
     set(
         ${ARG_DEST_VERSION_FILE_PATH}
         "${CONFIG_DIR}/DynamicObjectConfigVersion.cmake"
     )
     write_basic_package_version_file(
-        ${VERSION_FILE_PATH}
+        ${ARG_DEST_VERSION_FILE_PATH}
         VERSION "0.1"
         COMPATIBILITY AnyNewerVersion # Guaranteed after first release
     )
 
-    set(${ARG_DEST_CONFIG_FILE_PATH} "${CONFIG_DIR}/DynamicObjectConfig.cmake")
+    set(
+        ${ARG_DEST_CONFIG_FILE_PATH}
+        "${CONFIG_GENERATE_DIR}/DynamicObjectConfig.cmake"
+    )
     set(INCLUDE_DIRS "../../../include") # Relative path from config directory
     configure_package_config_file(
         "cmake_scripts/DynamicObjectConfig.cmake.in"
-        "${CONFIG_FILE_PATH}"
-        INSTALL_DESTINATION ${CONFIG_INSTALL_DIR}
+        "${ARG_DEST_CONFIG_FILE_PATH}"
+        INSTALL_DESTINATION CONFIG_INSTALL_DIR
     )
+
+    set(${ARG_DEST_CONFIG_INSTALL_DIR} ${CONFIG_INSTALL_DIR})
 
     return(
         PROPAGATE
